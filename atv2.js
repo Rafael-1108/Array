54012da7294a08a5d7f9732cb7c0e6f4931197e1
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-25 09:41:52 -0300

    atividade 2

---
+++ atv2.js
@@ -0,0 +1,6 @@
+let musicas = ["Tung Sahur Nela", "Menino de Vila", "505", "Nosso Amor"];
+let quantidade = musicas.length;
+    
+    for (let i = 0; i < quantidade; i++) {
+        console.log(musicas[i]);
+    }
