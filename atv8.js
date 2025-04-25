5c12348d9d0a842db1f5a21bf2497f272645b1d0
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-25 11:07:06 -0300

    atividade 8

---
+++ atv8.js
@@ -0,0 +1,10 @@
+let idades = ["15", "18", "35", "90", "12"];
+let quantidade = idades.length;
+
+for (let i = 0; i < quantidade; i++){
+    if (idades[i] >= 18){
+    console.log(idades[i]);
+    }
+}
+
+//Exibindo idades maiores que 18;
