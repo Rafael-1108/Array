4fedd3bbb135e2072ab37556a5b9fdc6a41b7267
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-25 10:32:09 -0300

    atividade 6

---
+++ atv6.js
@@ -0,0 +1,8 @@
+let itens = ["Pao", "Presunto", "Salsisha", "Queijo"];
+let contador = itens.length;
+
+    for(let i = (contador - 1); i >= 0; i--){
+        console.log(itens[i]);
+    }
+
+//Exibindo itens de trás para frente;
