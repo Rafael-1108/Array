5093b94201f0e6ac40975cd0311a8b0dd308ffc5
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-25 11:14:56 -0300

    atividade 9

---
+++ atv9.js
@@ -0,0 +1,6 @@
+let frutas = ["Abacaxi", "Limão", "Melancia", "Laranja", "Mamão", "Pêra"];
+let quantidadeFrutas = frutas.length;
+
+console.log("Tem ", quantidadeFrutas, " frutas.");
+
+//Exibindo a quantidade de frutas;
